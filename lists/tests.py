80973063b2f4164374892f993f32667a55ec477a
Арслан <arslan_92@mail.ru>
2019-12-16 14:40:43 +0700

    Первый модульный тест и преобразование url

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''смоук-тест бля'''
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
